aa11f59df5506e83d217c4329c147eeaaa666fff
Dāvis Sparinskis <davis.sparinskis@gmail.com>
2020-03-26 23:42:20 +0200

    send pinned comment contents

--- apexBot.py
+++ apexBot.py
@@ -11,6 +11,8 @@ STATS_CHANNEL = os.getenv('STATS_CHANNEL')
 
 bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
+pin = None
+
 
 def check_channel(channel):
     return channel.name == STATS_CHANNEL
@@ -25,6 +27,7 @@ async def on_ready():
 
     pins = await channel.pins()
 
+    global pin
     pin = discord.utils.get(pins, author=bot.user)
     if pin is None:
         message = await channel.send('stats')
@@ -35,7 +38,7 @@ async def on_ready():
 async def stats(ctx):
     if not check_channel(ctx.channel):
         return
-    await ctx.send('stats')
+    await ctx.send(pin.content)
 
 
 bot.run(TOKEN)
